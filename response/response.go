46b5fae45155cc89eaef457bf25b95239008aac8
Aditia Pratama <aditiapratamagg@gmail.com>
2020-03-16 14:52:39 +0700

    Add pagination response

--- response/response.go
+++ response/response.go
@@ -5,6 +5,15 @@ type BaseResponse struct {
 	Message string `json:"message"`
 }
 
+type PaginationResponseData struct {
+	CurrentPage    int `json:"current_page"`
+	PageCount      int `json:"page_count"`
+	PageSize       int `json:"page_size"`
+	RowCount       int `json:"total"`
+	FirstRowOnPage int `json:"first_row_on_page"`
+	LastRowOnPage  int `json:"last_row_on_page"`
+}
+
 func SetMessage(msg string, scss bool) BaseResponse {
 	return BaseResponse{
 		Message: msg,
